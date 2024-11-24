a1e62e86fa28764895088b4330b96b651b06fc27
Roby Setyawan <robby.setyawan@bni.co.id>
2024-11-24 20:32:26 +0700

    TwoSum add testCase

--- twosums/twosum_test.go
+++ twosums/twosum_test.go
@@ -11,9 +11,9 @@ func TestTwoSum(t *testing.T) {
 	runTestCase(t, []int{2, 3, 1}, 3, []int{2, 0}, "should return 2,0 when given 2,3,1 and target is 3")
 	runTestCase(t, []int{1, 2, 3}, 4, []int{0, 2}, "should return 0,2 when given 1,2,3 and target is 4")
 	runTestCase(t, []int{4, 2, 3}, 6, []int{1, 0}, "should return 1,0 when given 4,2,3 and target is 6")
-	runTestCase(t, []int{4, 2, 3, 1, 5}, 9, []int{0, 4}, "should return 1,0 when given 4,2,3,1,5 and target is 9")
-	runTestCase(t, []int{5, 2, 3, 1, 4}, 9, []int{4, 0}, "should return 1,0 when given 4,2,3,1,5 and target is 9")
-	runTestCase(t, []int{2, 2, 5, 1, 4}, 9, []int{4, 2}, "should return 1,0 when given 4,2,3,1,5 and target is 9")
+	runTestCase(t, []int{4, 2, 3, 1, 5}, 9, []int{0, 4}, "should return 0,4 when given 4,2,3,1,5 and target is 9")
+	runTestCase(t, []int{5, 2, 3, 1, 4}, 9, []int{4, 0}, "should return 4,0 when given 5, 2, 3, 1, 4 and target is 9")
+	runTestCase(t, []int{2, 2, 7, 1, 4}, 11, []int{4, 2}, "should return 4,2 when given 2, 2, 5, 1, 4 and target is 11")
 }
 
 func runTestCase(t *testing.T, nums []int, target int, want []int, description string) {
